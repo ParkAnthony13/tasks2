e3ed2583e594925e7e1e88ade79dc579e0762ca0
ParkAnthony13 <anth0nypark13@gmail.com>
2022-01-14 02:20:07 -0800

    Designing checkbox

--- client/src/components/Test.jsx
+++ client/src/components/Test.jsx
@@ -4,33 +4,25 @@ import '../css/Test.css';
 
 
 const Test = props => {
-    const [x, setX] = useState(400);
-    const [y,setY] = useState(110);
-    const [startx, setstartx] = useState();
-    const [starty, setstarty] = useState();
-    const [finalX, setFinalX] = useState('');
-    const [finalY, setFinalY] = useState('');
 
-    useEffect(()=>{
-        setFinalX(x + 'px');
-        setFinalY(y + 'px');
-    },[x,y])
-    const resizeX = () => {
-
-    }
-    const test = (event) => {
-        setstartx(event.clientX - event.target.offsetLeft)
-        console.log(`Horizontal: ${event.clientX - event.target.offsetLeft}`);
-        console.log(`Vertical: ${event.clientY - event.target.offsetTop}`)
-    }
 
 
     return(
         <div className='testContainer'>
-            <div className='taskCard' style={{width:`${finalX}`,height:`${finalY}`}}>
-                <div className='resize'>
-                    <div className='resizeTab' onMouseDown={test}/>
-                </div>
+            <div className='taskCard' style={{width:'205px'}}>
+                <section className='inProgress dailyLeft'>
+                    <div className='checkContainer'>
+                        <input type="checkbox"  className='checkbox'/>
+                        <span className='checkTypeA'></span>
+                    </div>
+                </section>
+                <article>text</article>
+                <section className='dailyRight'>
+                    <div>...</div>
+                    <div>x</div>
+                </section>
+            </div>
+            <div className='taskCard' style={{width:'500px'}}>
                 <section className='inProgress dailyLeft'></section>
                 <article>text</article>
                 <section className='dailyRight'>
